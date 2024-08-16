ef2ac233517897ae685d9e3706fc360eaaece0df
ricardosvale <ricardost3@gmail.com>
2024-08-16 11:29:32 -0300

    Initial Commit

---
+++ POC-Registro-Ponto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC-Registro-Ponto
+//
+//  Created by Ricardo Silva Vale on 16/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC-Registro-Ponto/POC_Registro_PontoApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_Registro_PontoApp.swift
+//  POC-Registro-Ponto
+//
+//  Created by Ricardo Silva Vale on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct POC_Registro_PontoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
