763a8ba623c69a9291006a3fb36ecc6da370f773
ricardosvale <ricardost3@gmail.com>
2024-08-16 11:37:18 -0300

    Feat: "Registro de Ponto"

---
+++ POC-Registro-Ponto/PointRegister.swift
@@ -0,0 +1,18 @@
+//
+//  PoointRegister.swift
+//  POC-Registro-Ponto
+//
+//  Created by Ricardo Silva Vale on 16/08/24.
+//
+
+import SwiftUI
+
+struct PoointRegister: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoointRegister()
+}
